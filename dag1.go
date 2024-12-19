aa9de911cda7574f52890b622237cbc58fda7446
LoomsyTiger <juliusheenk@gmail.com>
2024-12-19 22:09:13 +0100

    added slices to sort columns

--- dag1.go
+++ dag1.go
@@ -1,9 +1,34 @@
 package main
 
 import "fmt"
+import "os"
+import "bufio"
+import "strings"
 
 func main() {
-	const a int32 = 12
-	const b float32 = 20.5
-	fmt.Println("Test")
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("Fout bij openen bestand: ", err)
+	}
+	defer file.Close()
+
+	leftSlice := []string{}
+	rightSlice := []string{}
+	
+	r := bufio.NewReader(file)
+	for {
+		line, _, err := r.ReadLine()
+		if len(line) > 0 {
+			lineString := string(line)
+			split := strings.Split(lineString, "   ")
+			leftSlice = append(leftSlice, split[0])
+			rightSlice = append(rightSlice, split[1])
+			fmt.Println("Left: ", split[0])
+			fmt.Println("Right: ", split[1])
+		}
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+	}
 }
