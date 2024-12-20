1707aa60613decfdac3f04d864702dff6f4082eb
Julius Heenk <juliush@topdesk.com>
2024-12-20 11:15:54 +0100

    completed similarity score

--- dag1/dag1.go
+++ dag1/dag1.go
@@ -1,28 +1,52 @@
 package main
 
-import "fmt"
-import "os"
-import "bufio"
-import "strings"
-import "slices"
-import "strconv"
-import "sort"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
 
 func diff(a, b int) int {
 	if a < b {
-		return b - a 
+		return b - a
 	}
 	return a - b
 }
 
 func sum(nums []int) int {
 	total := 0
-
 	for _, num := range nums {
 		total += num
 	}
-
 	return total
+}
+
+func findSimilarity(a, b []int) map[int]int {
+	// gebruik een for loop om te controleren hoe veel waarden in de andere lijst matchen. bij een match, doe i++. eindwaarde van i is het aantal matches
+	allMatches := map[int]int{}
+	for _, aNum := range a {
+		matches := 0
+		for _, bNum := range b {
+			if aNum == bNum {
+				matches++
+			}
+			score := aNum * matches
+			allMatches[aNum] = score
+		}
+	}
+
+	return allMatches
+}
+
+func findSimilarityScore(x map[int]int) int {
+	totalScore := 0
+	for _, score := range x {
+		totalScore += score
+	}
+	return totalScore
 }
 
 func main() {
@@ -34,7 +58,7 @@ func main() {
 
 	leftSlice := []int{}
 	rightSlice := []int{}
-	
+
 	r := bufio.NewReader(file)
 	for {
 		line, _, err := r.ReadLine()
@@ -42,6 +66,9 @@ func main() {
 			lineString := string(line)
 			split := strings.Split(lineString, "   ")
 			splitLeft, err := strconv.Atoi(split[0])
+			if err != nil {
+				fmt.Println("Error: ", err)
+			}
 			splitRight, err := strconv.Atoi(split[1])
 			if err != nil {
 				fmt.Println("Error: ", err)
@@ -58,19 +85,19 @@ func main() {
 	}
 
 	distances := []int{}
-	similarityList := []int{}
 	for i, leftValue := range leftSlice {
 		rightValue := rightSlice[i]
 		diff := diff(leftValue, rightValue)
 		distances = append(distances, diff)
 
-// gebruik een for loop om te controleren hoe veel waarden in de andere lijst matchen. bij een match, doe i++. eindwaarde van i is het aantal matches
+		summedDistances := sum(distances)
+		fmt.Println("Summed distances: ", summedDistances)
 
-		similarityList = append(similarityList, similarity)
 	}
 
-	summedDistances := sum(distances)
-	fmt.Println("Summed distances: ", summedDistances)
+	similarity := findSimilarity(leftSlice, rightSlice)
+	fmt.Println(similarity)
 
-
+	finalScore := findSimilarityScore(similarity)
+	fmt.Printf("Similarity score: %d", finalScore)
 }
